ebe6f676819c64895d1344b4c59ba680da8f148a
Your name <Your email>
2025-01-28 12:39:34 +0100

    tickethackbackend

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fetch = require("node-fetch");
+require('../models/connection');
+const Trip = require('../models/trips');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post('/', (req, res) => {
+  if (req.body.departure.trim() === '' || req.body.arrival.trim() === '') {
+      return res.json({ result: false, error: 'Trips not found' });
+  }
+  Trip.find({ 
+      departure: req.body.departure, 
+      arrival: req.body.arrival, 
+      date: req.body.date
+  }).then(data => {
+      if (!data) {
+          return res.json({ result: false, error: 'Trips not found' });
+      }
+
+      res.json({result: true, allTrips: data});
+  })
 });
 
 module.exports = router;
