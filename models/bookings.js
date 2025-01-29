9695439c116381e6371e6c422af424221764840b
Your name <Your email>
2025-01-29 09:48:02 +0100

    tickethack-backend

--- models/bookings.js
+++ models/bookings.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bookingSchema = mongoose.Schema({
   cart: { type: mongoose.Schema.Types.ObjectId, ref: 'carts', required: true },
+  trip: { type: mongoose.Schema.Types.ObjectId, ref: 'trips', required: true }
 });
 
 const Booking = mongoose.model('bookings', bookingSchema);
--- routes/bookings.js
+++ routes/bookings.js
@@ -2,35 +2,42 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Cart = require('../models/carts');
-// const Trip = require('../models/trips');
+const Trip = require('../models/trips');
 const Booking = require('../models/bookings');
 
 //Pour ajouter les achats
 router.post('/save', (req, res) => {
-    const cartId = req.body.id;
-    if (!cartId) {
-      return res.json({ result: false, error: 'Missing cart ID' });
-    }
-    Cart.findById(cartId)
-      .then(cartData => {
-        if (!cartData) {
-          return res.json({ result: false, error: 'CART not found' });
-        }
-        return Booking.create({ cart: cartData._id });
-      })
-      .then(newBookingItem => {
-        return Booking.findById(newBookingItem._id)
-          .populate('cart'); 
-      })
-      .then(populatedBookingItem => {
-        res.json({ result: true, booking: populatedBookingItem });
-      })
+  const cartId = req.body.id;
+  if (!cartId) {
+    return res.json({ result: false, error: 'Missing cart ID' });
+  }
+  Cart.findById(cartId)
+    .then(cartData => {
+      if (!cartData) {
+        return res.json({ result: false, error: 'Cart not found' });
+      }   
+      return Booking.create({ 
+        cart: cartData._id,
+        trip: cartData.trip
+      }).then(newBookingItem => {
+        return Cart.findByIdAndDelete(cartId)
+          .then(() => newBookingItem);
+      });
+    })
+    .then(newBookingItem => {
+      return Booking.findById(newBookingItem._id)
+        .populate('trip');
+    })
+    .then(populatedBookingItem => {
+      res.json({ result: true, booking: populatedBookingItem });
+    })
+});
+  
+// Tous les achats
+  router.get("/", (req, res) => {
+    Booking.find().then(data => {
+      res.json({ booking: data });
+    });
   });
-  
-  
-
-
-
-
 
 module.exports = router;
